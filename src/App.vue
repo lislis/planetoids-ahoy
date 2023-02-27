532880cc0a5c1449ec2edc51b189a237cdcd5630
lislis <mail@lislis.de>
2023-02-27 23:32:19 +0100

    add gh link

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,6 @@ import { RouterLink, RouterView } from 'vue-router'
   <RouterView />
 
   <footer class="l-footer">
-      <p>Made with &lt;3 by <a href="https://toot.cat/@lislis" target="_blank">lislis</a>. Data from <a href="https://api.nasa.gov/" target="_blank">NASA</a>. Code available on <a href="">GitHub</a> under <a href="https://github.com/lislis/planetoids-ahoy/blob/main/LICENSE" target="_blank">MIT</a> license.</p>
+      <p>Made with &lt;3 by <a href="https://toot.cat/@lislis" target="_blank">lislis</a>. Data from <a href="https://api.nasa.gov/" target="_blank">NASA</a>. Code available on <a href="https://github.com/lislis/planetoids-ahoy">GitHub</a> under <a href="https://github.com/lislis/planetoids-ahoy/blob/main/LICENSE" target="_blank">MIT</a> license.</p>
   </footer>
 </template>
